77ef171a79651bd3f2ad7559cd1229a29d5f0615
manelpalacdi <manelpalacdi@gmail.com>
2024-06-26 21:11:40 +0200

    subplots

--- product_analysis/main.py
+++ product_analysis/main.py
@@ -31,20 +31,36 @@ def data_plot(df: pd.DataFrame, prod_id: str):
 
     plt.figure(figsize=(10, 6))
 
-    plt.subplot(1, 3, 1)
-    plt.plot(df_prod["date"], df_prod["price"], label="price")
-    plt.xticks(df_prod["date"].iloc[::3])
+    # we get normalized values for the merged subplot
+    norm_df_prod = df_prod.copy()
+    norm_df_prod["price"] = (norm_df_prod["price"] - norm_df_prod["price"].min()) /  (norm_df_prod["price"].max() - norm_df_prod["price"].min())
+    norm_df_prod["reviews"] = (norm_df_prod["reviews"] - norm_df_prod["reviews"].min()) /  (norm_df_prod["reviews"].max() - norm_df_prod["reviews"].min())
+    norm_df_prod["sales"] = norm_df_prod["sales"].astype(float)
+    norm_df_prod["sales"] = (norm_df_prod["sales"] - norm_df_prod["sales"].min()) /  (norm_df_prod["sales"].max() - norm_df_prod["sales"].min())
+
+    plt.subplot(2, 1, 1)
+    plt.plot(norm_df_prod["date"], norm_df_prod["price"], color="red", label="price", marker=".")
+    plt.plot(norm_df_prod["date"], norm_df_prod["reviews"], color="green", label="reviews", marker=".")
+    plt.plot(norm_df_prod["date"], norm_df_prod["sales"], color="orange", label="sales", marker=".")
     plt.legend()
 
-    plt.subplot(1, 3, 2)
-    plt.plot(df_prod["date"], df_prod["reviews"], label="reviews")
+    plt.subplot(2, 3, 4)
+    plt.bar(df_prod["date"], df_prod["price"], alpha=0.5)
+    plt.plot(df_prod["date"], df_prod["price"], label="price", color="red", marker=".")
     plt.xticks(df_prod["date"].iloc[::3])
-    plt.legend()
+    plt.legend(loc="upper right")
+
+    plt.subplot(2, 3, 5)
+    plt.bar(df_prod["date"], df_prod["reviews"].astype(float), alpha=0.5)
+    plt.plot(df_prod["date"], df_prod["reviews"].astype(float), label="reviews", color="green", marker=".")
+    plt.xticks(df_prod["date"].iloc[::3])
+    plt.legend(loc="upper right")
     
-    plt.subplot(1, 3, 3)
-    plt.plot(df_prod["date"], df_prod["sales"], label="sales")
+    plt.subplot(2, 3, 6)
+    plt.bar(df_prod["date"], df_prod["sales"].astype(float), alpha=0.5)
+    plt.plot(df_prod["date"], df_prod["sales"].astype(float), label="sales", color="orange", marker=".")
     plt.xticks(df_prod["date"].iloc[::3])
-    plt.legend()
+    plt.legend(loc="upper right")
 
     # rotate date labels so they fit better
     plt.gcf().autofmt_xdate()
